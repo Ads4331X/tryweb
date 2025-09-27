a01b4add3fc817069695c5bc8f83a34fac5c37da
ads4331X <karkitan@gmail.com>
2025-09-27 20:24:59 +0545

    ui change

--- src/pages.jsx
+++ src/pages.jsx
@@ -23,7 +23,7 @@ function Pages(props) {
       >
         Pages
       </a>
-      <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+      <div className="dropdown-menu " aria-labelledby="dropdownMenuLink">
         {dropdown_info.map((info, key) => (
           <a className="dropdown-item" href="#" key={key}>
             {info}
