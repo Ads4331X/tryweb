728176003f6753507f4a96b5afffee32c3213e90
ads4331X <karkitan@gmail.com>
2025-09-27 20:15:20 +0545

    added new details

--- src/components/subcontent/aboutus.jsx
+++ src/components/subcontent/aboutus.jsx
@@ -1,18 +1,38 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./aboutus.css";
 function AboutUs() {
   return (
-    <div className="sub-content container-fluid about-us p-3 d-flex align-items-center flex-column flex-md-row">
-      <div>
-        <img
-          src="https://themewagon.github.io/Edukate/img/about.jpg"
-          className="w-100 h-100"
-        />
-      </div>
-      <div className="about-text">
-        <span>ABOUT US</span>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={12} md={12} lg={5}>
+          <div className="about-image">
+            <img
+              src="https://themewagon.github.io/Edukate/img/about.jpg"
+              className="img-fluid"
+            />
+          </div>
+        </Col>
+        <Col sm={12} md={12} lg={7} className="about-text">
+          <h6 className=" fs-5 about-text">About Us</h6>
+          <div className=" info ">
+            <h1 className="text-dark display-4 fw-bold mb-4 ">
+              First Choice For Online Education Anywhere
+            </h1>
+            <p className="text-secondary mb-4 about-para">
+              Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam
+              dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam
+              diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem
+              et sit, sed stet no labore lorem sit. Sanctus clita duo justo et
+              tempor consetetur takimata eirmod, dolores takimata consetetur
+              invidunt magna dolores aliquyam dolores dolore. Amet erat amet et
+              magna
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 export default AboutUs;
---
+++ src/components/subcontent/autocounter.jsx
@@ -0,0 +1,10 @@
+function AutoCounter() {
+  let [count, setcount] = useState(0);
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default AutoCounter;
