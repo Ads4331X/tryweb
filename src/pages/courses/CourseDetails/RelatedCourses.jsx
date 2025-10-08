5245b56753cf5e53b30bad454987d6a9bbdc7a37
ads4331X <karkitan@gmail.com>
2025-10-08 22:55:05 +0545

    test

--- src/pages/courses/CourseDetails/RelatedCourses.jsx
+++ src/pages/courses/CourseDetails/RelatedCourses.jsx
@@ -13,71 +13,71 @@ import { RiArrowLeftSLine } from "react-icons/ri";
 
 function RelatedCourses() {
   return (
-    <Container className="position-relative">
-        <div className={css.ButtonContainer}>
-          <div
-            className={`swiper-button-prev ${css.prev}  text-light fw-bold fs-4`}
-          >
-            <RiArrowLeftSLine />
-          </div>
-          <div
-            className={`swiper-button-next ${css.next} text-white fw-bold fs-4`}
-          >
-            <RiArrowRightSLine />
-          </div>
-        </div>
-        <Swiper
-          modules={[Navigation]}
-          navigation={{
-            prevEl: ".swiper-button-prev",
-            nextEl: ".swiper-button-next",
-          }}
-          loop={true}
-          slidesPerView={1}
-          spaceBetween={30}
-          breakpoints={{
-            768: {
-              slidesPerView: 2,
-            },
-          }}
+    <Container className="position-relative  mb-5">
+      <div className={css.ButtonContainer}>
+        <div
+          className={`swiper-button-prev ${css.prev}  text-light fw-bold fs-4`}
         >
-          {courseDataWithIds.map((course) => (
-            <SwiperSlide
-              className={` text-light ${css.ScalerContainer}} `}
-              key={course.id}
-            >
-              <div className={css.Scaler}>
-                <div className={css.ImgContainer}>
-                  <img
-                    src={course.bgImg}
-                    alt={course.title}
-                    className="w-100 h-100 img-fluid"
-                  />
-                </div>
-                <div className={css.overlay} />
-                <div className={css.courseCard}>
-                  <h3 className="p-3 fs-4 text-center">{course.coursename}</h3>
-                  <div className="border-bottom" />
-                  <div className="d-flex justify-content-between  p-3">
-                    <h4 className="fs-6">
-                      <FaUser /> &nbsp;
-                      {course.Instructor}
-                    </h4>
-                    <div>
-                      <span className="fs-6">
-                        <FaStar /> &nbsp;
-                        {course.rating}&nbsp;
-                        <small className="fw-normal">
-                          ({course.ratednumber})
-                        </small>
-                      </span>
-                    </div>
+          <RiArrowLeftSLine />
+        </div>
+        <div
+          className={`swiper-button-next ${css.next} text-white fw-bold fs-4`}
+        >
+          <RiArrowRightSLine />
+        </div>
+      </div>
+      <Swiper
+        modules={[Navigation]}
+        navigation={{
+          prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next",
+        }}
+        loop={true}
+        slidesPerView={1}
+        spaceBetween={30}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+          },
+        }}
+      >
+        {courseDataWithIds.map((course) => (
+          <SwiperSlide
+            className={` text-light ${css.ScalerContainer}} `}
+            key={course.id}
+          >
+            <div className={css.Scaler}>
+              <div className={css.ImgContainer}>
+                <img
+                  src={course.bgImg}
+                  alt={course.title}
+                  className="w-100 h-100 img-fluid"
+                />
+              </div>
+              <div className={css.overlay} />
+              <div className={css.courseCard}>
+                <h3 className="p-3 fs-4 text-center">{course.coursename}</h3>
+                <div className="border-bottom" />
+                <div className="d-flex justify-content-between  p-3">
+                  <h4 className="fs-6">
+                    <FaUser /> &nbsp;
+                    {course.Instructor}
+                  </h4>
+                  <div>
+                    <span className="fs-6">
+                      <FaStar /> &nbsp;
+                      {course.rating}&nbsp;
+                      <small className="fw-normal">
+                        ({course.ratednumber})
+                      </small>
+                    </span>
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Container>
   );
 }
