7ae7267dd87800d7946b1400cdd4963d2cb505f0
ads4331X <karkitan@gmail.com>
2025-10-09 20:50:35 +0545

    test

--- src/pages/courses/CourseDetails/CourseDetailPage.jsx
+++ src/pages/courses/CourseDetails/CourseDetailPage.jsx
@@ -1,11 +1,13 @@
 import Header from "../../../components/layout/header/header";
+import GotoTop from "../../../components/subcontent/gototop";
 import Content from "../../../content/content";
-import CourseDetail from "../../../pages/courses/CourseDetails/CourseDetail";
+import CourseDetail from "./CourseDetail";
 import Footer from "../../../components/layout/footer/Footer";
 function CourseDetailPage() {
   return (
     <>
       <Header currentpage="Course Detail" />
+      <GotoTop />
       <Content
         Title="Course Detail"
         TitleTag="h1"
